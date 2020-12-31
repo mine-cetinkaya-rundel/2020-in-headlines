a38dd30ee9c61515531b7bef0dab572b02f0fb67
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-12-31 16:32:55 +0000

    Deploy script

---
+++ deploy.R
@@ -0,0 +1,18 @@
+rsconnect::deployApp(
+  appName = "2020-in-headlines",
+  appTitle = "2020 in headlines",
+  account = "minecr",
+  forceUpdate = TRUE,
+  launch.browser = TRUE,
+  appFiles = c("app.R",
+               "data/top-articles.rds",
+               "data/top-words-nested.rds",
+               "www/2020.png",
+               "www/headlines.css",
+               "www/icons8-poo.svg")
+)
+
+# instance size - largest possible
+# max worker - 2
+# max connections - 20
+# start count - 25
